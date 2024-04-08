b5c7e627c3de70e0018374b315420afcbb7b0b81
JLXP <javier.xool@outlook.com>
2024-04-08 11:05:59 -0600

    firt commit

---
+++ app/Models/Vacante.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vacante extends Model
+{
+    use HasFactory;
+
+    //Ya se no se usa dates
+    protected $casts = ['ultimo_dia' => 'date'];
+
+    protected $fillable = [
+        'titulo',
+        'salario_id',
+        'categoria_id',
+        'empresa',
+        'ultimo_dia',
+        'descripcion',
+        'imagen',
+        'user_id'
+    ];
+}
