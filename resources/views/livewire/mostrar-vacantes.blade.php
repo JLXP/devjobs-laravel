0316dea1663c57d7ea14f17072d2cf4ffe16ce8a
JLXP <javier.xool@outlook.com>
2024-05-15 14:52:31 -0600

    Mostrando una vacante en el Frontend

--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -4,7 +4,7 @@
         @forelse ($vacantes as $vacante)
             <div class="p-6 bg-white border-b border-gray-200 md:flex md:justify-between md:items-center">
                 <div class="leading-10">
-                    <a href="#" class="text-xl font-bold">
+                    <a href="{{ route('vacantes.show', $vacante->id) }}" class="text-xl font-bold">
                         {{ $vacante->titulo }}
                     </a>
                     <p class="text-sm text-gray-600 font-bold">{{ $vacante->empresa }}</p>
@@ -53,7 +53,7 @@
             }).then((result) => {
                 if (result.isConfirmed) {
                     // ELiminar vacante
-                    @this.call('eliminarVacante',vacanteId);
+                    @this.call('eliminarVacante', vacanteId);
                     Swal.fire(
                         'Se elimino la vacante!',
                         'Eliminado Correctamente',
